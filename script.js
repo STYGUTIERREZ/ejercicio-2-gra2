04dad7b8ef48e0b023461eb41217392fee18a260
Jordin Gutierrez <sjgutierrez2016@gmail.com>
2024-06-09 16:04:39 -0500

    ejercicio gra2

---
+++ script.js
@@ -0,0 +1,8 @@
+let grados_centigrados = 30
+let grados_farenheit;
+let suma = 32
+let multiplicacion = 1.8
+
+grados_farenheit = grados_centigrados * multiplicacion + suma 
+
+alert(`grados farenheit igual a ${grados_farenheit}`);
